e961c613d41659bc497a9d4022a2c7ec5d738a27
Irwin <rodriguez.irwin@gmail.com>
2021-04-16 12:12:21 +0200

    first commit

---
+++ Chapter 1/game_over2.cpp
@@ -0,0 +1,10 @@
+// Game Over 2.0
+// Demostrates a using directive
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Game Over 2.0!" << endl;
+    return 0;
+}
---
+++ Chapter 1/game_over3.cpp
@@ -0,0 +1,11 @@
+// Game Over 3.0
+// Demostrates using declarations
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "Game Over!" << endl;
+    return 0;
+}
